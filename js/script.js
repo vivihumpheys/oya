7ea018e5d4258acabc8d204358b968691b0c28e8
Viviane Miranda <vivianehfm@gmail.com>
2020-05-22 13:23:20 -0300

    site ok, falta apenas corrigir ancora da nav

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// window.onload = function(e) {
+//     var offset = document.getElementsByClassName('nav-container')[0].offsetTop;
+//     var menu = document.getElementsByClassName('nav-container')[0];
+    
+//     console.log ( menu )
+
+//     document.addEventListener('scroll', function() {
+//         if (document.body.scrollTop > offset || document.documentElement.scrollTop > offset) {
+//             menu.style.position = 'fixed';
+//         } else {
+//             menu.style.position = 'initial';
+//         }
+//     });
+// }
+
+
